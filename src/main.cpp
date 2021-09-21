d61ee4a998a11ff65f50520b46a8bfa5d1013fd4
andy <abondarew@gmail.com>
2021-09-22 00:35:17 +0300

    qqq

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  delay(12);
+  digitalWrite(12,1);
+  digitalWrite(12, 1);
+  analogWrite(1, 0);
+  int number = 0;
+  char c = 3;
+  int f = number+c;
+
+
+
+  // put your main code here, to run repeatedly:
+}
